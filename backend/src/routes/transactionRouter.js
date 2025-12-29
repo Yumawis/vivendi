14d5f296cefc054577c7e05fe91866a5f69566f4
Yury Mayerly Martinez Diaz <yumawis.io@gmail.com>
2025-12-28 22:57:43 -0500

    The transaction controller was imported into your routes file.

--- backend/src/routes/transactionRouter.js
+++ backend/src/routes/transactionRouter.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const { addTransaction } = require("../controllers/transactionController");
+
 router.put("/property/:userId", addTransaction);
 
 module.exports = router;
